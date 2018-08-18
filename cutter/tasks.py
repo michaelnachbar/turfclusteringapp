9c36e27036b9605a4c83ea4cd5af03652593bd77
Mike Nachbar <michael.l.nachbar@gmail.com>
2018-08-18 18:29:17 -0500

    Moving variable len dict outside of for loop

--- cutter/tasks.py
+++ cutter/tasks.py
@@ -334,8 +334,8 @@ def add_region(form):
                     string_len_dict[col] = max(voter_data[col].str.len().max(),10)
                 else:
                     string_len_dict[col] = max(string_len_dict[col],voter_data[col].str.len().max())  
-            for col in string_len_dict.keys():
-                varchar_len_dict[col] = sqlalchemy.types.NVARCHAR(length=string_len_dict[col])
+        for col in string_len_dict.keys():
+            varchar_len_dict[col] = sqlalchemy.types.NVARCHAR(length=string_len_dict[col])
         varchar_len_dict["full_street"] = sqlalchemy.types.NVARCHAR(length=1000)
         varchar_len_dict["address"] = sqlalchemy.types.NVARCHAR(length=1000)
         varchar_len_dict["address_exp"] = sqlalchemy.types.NVARCHAR(length=1000)
